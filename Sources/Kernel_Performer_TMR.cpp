f1bdd13885cdd1a1a5457b3c956339cf6d8bbbc6
KGN <koban4ik_11@mail.ru>
2018-10-16 10:37:33 +0300

    1) Класс Kernel_Performer_tmr_t: корректировка метода Add_OneTime_Event. Счётчик очереди записывается во временную переменную. Добавлена проверка на NULL при вызове callback.

--- Sources/Kernel_Performer_TMR.cpp
+++ Sources/Kernel_Performer_TMR.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "Kernel_Performer_TMR.hpp"
+#include "CONTROLLER.hpp"
 
 
 
@@ -14,9 +15,13 @@ Kernel_Performer_tmr_t::Kernel_Performer_tmr_t(VOID_TIMER_t* VOID_TMR_ptr):Event
 	this->Event_br = 0;
 	this->Event_bw = 0;
 }
+volatile Callback_Interface_t* tmpCall;
+volatile void* tmpData;
 
 void Kernel_Performer_tmr_t::void_callback(void* Intf_ptr, void* parameters)
 {
+	CONTROLLER_t::get_CONTROLLER()->GPIO_ON(LED4);
+	//CONTROLLER_t::get_CONTROLLER()->GPIO_ON(LED5);
 	uint32_t temp_bw;
 	uint32_t temp_br;
 
@@ -27,9 +32,13 @@ void Kernel_Performer_tmr_t::void_callback(void* Intf_ptr, void* parameters)
 
 	while(temp_bw != temp_br)
 	{
-		if(this->callbacks[temp_br] != (Callback_Interface_t*)NULL)
+		tmpCall = this->callbacks[temp_br];
+		if(((Callback_Interface_t*) tmpCall) != (Callback_Interface_t*)NULL)
 		{
-			this->callbacks[temp_br]->void_callback((void*)this, this->Event_data[temp_br]);
+			tmpData = (void*) this->Event_data[temp_br];
+			if(this->Event_data[temp_br] != NULL) {
+				((Callback_Interface_t*) tmpCall)->void_callback((void*)this, this->Event_data[temp_br]);
+			}
 			this->callbacks[temp_br] = (Callback_Interface_t*)NULL;
 			this->Event_data[temp_br] = NULL;
 		}
@@ -37,16 +46,27 @@ void Kernel_Performer_tmr_t::void_callback(void* Intf_ptr, void* parameters)
 		temp_br = temp_br == Event_TMR_size_of_events ? 0 : temp_br;
 	}
 	this->Event_br = temp_br;
-
+	//CONTROLLER_t::get_CONTROLLER()->GPIO_OFF(LED5);
+CONTROLLER_t::get_CONTROLLER()->GPIO_OFF(LED4);
 }
 
 void Kernel_Performer_tmr_t::Add_OneTime_Event(uint32_t Delay_us, Callback_Interface_t* Callback, void* data)
 {
-	this->Event_data[this->Event_bw] = data;
-	this->callbacks[this->Event_bw] = Callback;
-	this->Event_bw++;
-	this->Event_bw = this->Event_bw == Event_TMR_size_of_events ? 0 : this->Event_bw;
+	uint32_t tempBw;
+	tempBw = this->Event_bw;
+
+	this->Event_data[tempBw] = data;
+	this->callbacks[tempBw] = Callback;
+	tempBw++;
+	tempBw = tempBw == Event_TMR_size_of_events ? 0 : tempBw;
+	this->Event_bw = tempBw;
 	this->Start();
+
+//	this->Event_data[this->Event_bw] = data;
+//	this->callbacks[this->Event_bw] = Callback;
+//	this->Event_bw++;
+//	this->Event_bw = this->Event_bw == Event_TMR_size_of_events ? 0 : this->Event_bw;
+//	this->Start();
 }
 
 void Kernel_Performer_tmr_t::Add_Cyclic_Event(uint32_t Period_us, Callback_Interface_t* Callback, void* data)
