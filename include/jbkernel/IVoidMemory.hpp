e688cc42d32acfc9b3efdced6101c1199f7717ab
Stalker1290 <Strelok1290@gmail.com>
2019-09-03 12:40:33 +0300

    Add interface for NVM

---
+++ include/jbkernel/IVoidMemory.hpp
@@ -0,0 +1,105 @@
+/**
+ * @file
+ * @brief IVoidMemory Interface Description
+ *
+ * Event Timer for cycling/one time events.
+ *
+ * @note
+ * Copyright © 2019 Evgeniy Ivanov. Contacts: <strelok1290@gmail.com>
+ * All rights reserved.
+ * @note
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * @note
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @note
+ * This file is a part of JB_Lib.
+ */
+
+#ifndef JBKERNEL_IVOIDMEMORY_HPP_
+#define JBKERNEL_IVOIDMEMORY_HPP_
+
+#include "jbkernel/jb_common.h"
+#if USE_FAT_FS
+#include "jbfatfs/IStorageDevice.hpp"
+#endif
+
+namespace jblib
+{
+namespace jbkernel
+{
+
+#if USE_FAT_FS
+using namespace jbfatfs;
+#endif
+
+
+
+#if USE_FAT_FS
+class IVoidMemory : public IStorageDevice
+#else
+class IVoidMemory
+#endif
+{
+public:
+	IVoidMemory(uint32_t baseAddress);
+	virtual ~IVoidMemory(void);
+	void read(uint32_t address, uint8_t* data, uint32_t size);
+	void write(uint32_t address, uint8_t* data, uint32_t size);
+	void enableCache(uint32_t cacheCellSize, uint8_t cacheSizeCells);
+	void disableCache(void);
+	void flushCache(void);
+	uint32_t getBaseAddess(void);
+	virtual void initialize(void) = 0;
+
+	#if USE_FAT_FS
+	virtual DSTATUS diskInitialize(void);
+	virtual DSTATUS diskStatus(void);
+	virtual DRESULT diskRead(BYTE* buff, DWORD sector, UINT count);
+	virtual DRESULT diskWrite(const BYTE* buff, DWORD sector, UINT count);
+	virtual DRESULT diskIoctl(BYTE cmd, void* buff);
+	#endif
+
+protected:
+	virtual void readMemory(uint32_t address, uint8_t* data, uint32_t size) = 0;
+	virtual void writeMemory(uint32_t address, uint8_t* data, uint32_t size) = 0;
+
+	uint32_t baseAddress_ = 0;
+	bool isInitialized_ = false;
+	#if USE_FAT_FS
+	DSTATUS diskStatus_ = STA_NOINIT;
+	uint32_t diskBaseAddress_ = 0;
+	uint32_t diskSize_ = 0;
+	uint32_t diskBlockSize_ = 0;
+	#endif
+
+private:
+	int isRangeInCache(uint32_t address, uint32_t size);
+	int isAddressInCache(uint32_t address);
+	int findCacheCell(void);
+	void flushCacheCell(int cellNumber);
+
+	typedef struct
+	{
+		uint32_t address = 0;
+		uint8_t* cache = NULL;
+		uint16_t useCounter = 0;
+	}CacheCell_t;
+	uint32_t cacheCellSize_ = 0;
+	uint8_t cacheSizeCells_ = 0;
+	CacheCell_t* cache_ = NULL;
+	bool isCacheEnabled_ = false;
+};
+
+}
+}
+
+#endif /* JBKERNEL_IVOIDMEMORY_HPP_ */
---
+++ src/jbkernel/IVoidMemory.cpp
@@ -0,0 +1,337 @@
+/**
+ * @file
+ * @brief IVoidMemory Interface Realization
+ *
+ * Event Timer for cycling/one time events.
+ *
+ * @note
+ * Copyright © 2019 Evgeniy Ivanov. Contacts: <strelok1290@gmail.com>
+ * All rights reserved.
+ * @note
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * @note
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @note
+ * This file is a part of JB_Lib.
+ */
+
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+#include "jbkernel/IVoidMemory.hpp"
+#include "string.h"
+#if USE_CONSOLE
+#include "stdio.h"
+#endif
+
+namespace jblib::jbkernel
+{
+#if USE_FAT_FS
+using namespace jbfatfs;
+#endif
+
+
+#if USE_FAT_FS
+IVoidMemory::IVoidMemory(uint32_t baseAddress) : IStorageDevice()
+#else
+IVoidMemory::IVoidMemory(uint32_t baseAddress)
+#endif
+{
+	this->baseAddress_ = baseAddress;
+}
+
+
+
+IVoidMemory::~IVoidMemory(void)
+{
+	if(this->isCacheEnabled_)
+		this->disableCache();
+}
+
+
+
+uint32_t IVoidMemory::getBaseAddess(void)
+{
+	return this->baseAddress_;
+}
+
+
+
+void IVoidMemory::enableCache(uint32_t cacheCellSize, uint8_t cacheSizeCells)
+{
+	if(!this->isCacheEnabled_){
+		this->cacheCellSize_ = cacheCellSize;
+		this->cacheSizeCells_ = cacheSizeCells;
+		this->cache_ = (CacheCell_t*)malloc_s(sizeof(CacheCell_t) * this->cacheSizeCells_);
+		if(!this->cache_){
+			printf("IVoid Memory Error: No heap for cache!\n");
+			return;
+		}
+		for(uint32_t i = 0; i < this->cacheSizeCells_; i++){
+			this->cache_[i].address = this->baseAddress_;
+			this->cache_[i].useCounter = 0;
+			this->cache_[i].cache = (uint8_t*)malloc_s(cacheCellSize);
+			if(!this->cache_[i].cache){
+				if(!i){
+					printf("IVoid Memory Error: No heap for cache!\n");
+					free_s(this->cache_);
+					return;
+				}
+				else{
+					printf("IVoid Memory Error: No heap for cache! Trunc to %lu cells\n", i);
+					this->cacheSizeCells_ = i;
+					break;
+				}
+			}
+			disableInterrupts();
+			this->readMemory(this->cache_[i].address, this->cache_[i].cache, this->cacheCellSize_);
+			enableInterrupts();
+		}
+		this->isCacheEnabled_ = true;
+	}
+}
+
+
+
+void IVoidMemory::read(uint32_t address, uint8_t* data, uint32_t size)
+{
+	disableInterrupts();
+	if(this->isCacheEnabled_){
+		int cell = 0;
+		if((cell = this->isRangeInCache(address, size)) >= 0){
+			memcpy(data, &this->cache_[cell].cache[address - this->cache_[cell].address], size);
+			this->cache_[cell].useCounter++;
+			enableInterrupts();
+			return;
+		}
+		else if((cell = this->isAddressInCache(address)) >= 0){
+			uint32_t cachedSize =
+					this->cache_[cell].address + this->cacheCellSize_ - address;
+			memcpy(data,
+					&this->cache_[cell].cache[address - this->cache_[cell].address], cachedSize);
+			this->cache_[cell].useCounter++;
+			address += cachedSize;
+			data += cachedSize;
+			size -= cachedSize;
+		}
+	}
+	this->readMemory(address, data, size);
+	enableInterrupts();
+}
+
+
+
+void IVoidMemory::write(uint32_t address, uint8_t* data, uint32_t size)
+{
+	disableInterrupts();
+	if(this->isCacheEnabled_){
+		int cell = 0;
+		if((cell = this->isRangeInCache(address, size)) >= 0){
+			memcpy(&this->cache_[cell].cache[address - this->cache_[cell].address],
+					data, size);
+			this->cache_[cell].useCounter++;
+			enableInterrupts();
+			return;
+		}
+		else if((cell = this->isAddressInCache(address)) >= 0){
+			uint32_t cachedSize =
+					this->cache_[cell].address + this->cacheCellSize_ - address;
+			memcpy(&this->cache_[cell].cache[address - this->cache_[cell].address], data, cachedSize);
+			this->cache_[cell].useCounter++;
+			address += cachedSize;
+			data += cachedSize;
+			size -= cachedSize;
+		}
+		if(size <= this->cacheCellSize_){
+			cell = findCacheCell();
+			if(cell >= 0){
+				this->flushCacheCell(cell);
+				this->cache_[cell].address = address;
+				this->readMemory(address + size,
+						&this->cache_[cell].cache[size], this->cacheCellSize_ - size);
+				memcpy(this->cache_[cell].cache, data, size);
+				this->cache_[cell].useCounter++;
+				enableInterrupts();
+				return;
+			}
+		}
+	}
+	this->writeMemory(address, data, size);
+	enableInterrupts();
+}
+
+
+
+void IVoidMemory::flushCacheCell(int cellNumber)
+{
+	if(this->isCacheEnabled_){
+		disableInterrupts();
+		this->writeMemory(this->cache_[cellNumber].address,
+				this->cache_[cellNumber].cache, this->cacheCellSize_);
+		this->cache_[cellNumber].useCounter = 0;
+		enableInterrupts();
+	}
+}
+
+
+
+void IVoidMemory::flushCache(void)
+{
+	if(this->isCacheEnabled_){
+		for(uint32_t i = 0; i < this->cacheSizeCells_; i++){
+			this->flushCacheCell(i);
+		}
+	}
+}
+
+
+
+int IVoidMemory::findCacheCell(void)
+{
+	int ret = -1;
+	if(this->isCacheEnabled_){
+		ret = 0;
+		for(uint32_t i = 1; i < this->cacheSizeCells_; i++){
+			if(this->cache_[ret].useCounter > this->cache_[i].useCounter){
+				ret = i;
+			}
+		}
+	}
+	return ret;
+}
+
+
+
+void IVoidMemory::disableCache(void)
+{
+	if(this->isCacheEnabled_){
+		this->flushCache();
+		this->isCacheEnabled_ = false;
+		for(uint32_t i = 0; i < this->cacheSizeCells_; i++){
+			free_s(this->cache_[i].cache);
+		}
+		free_s(this->cache_);
+	}
+}
+
+
+
+int IVoidMemory::isRangeInCache(uint32_t address, uint32_t size)
+{
+	int ret = -1;
+	for(uint32_t i = 0; i < this->cacheSizeCells_; i++){
+		if((address >= this->cache_[i].address) &&
+				(address + size) <= (this->cache_[i].address + this->cacheCellSize_)){
+			ret = i;
+		}
+	}
+	return ret;
+}
+
+
+
+int IVoidMemory::isAddressInCache(uint32_t address)
+{
+	int ret = -1;
+	for(uint32_t i = 0; i < this->cacheSizeCells_; i++){
+		if((address >= this->cache_[i].address) &&
+				address <= (this->cache_[i].address + this->cacheCellSize_)){
+			ret = i;
+		}
+	}
+	return ret;
+}
+
+
+
+#if USE_FAT_FS
+
+DSTATUS IVoidMemory::diskStatus(void)
+{
+	return this->diskStatus_;
+}
+
+
+
+DSTATUS IVoidMemory::diskInitialize(void)
+{
+	if(!this->isInitialized_)
+		this->initialize();
+	return this->diskStatus_;
+}
+
+
+
+DRESULT IVoidMemory::diskRead(BYTE* buff, DWORD sector, UINT count)
+{
+	if (this->diskStatus_ & STA_NOINIT)
+		return RES_NOTRDY;
+	uint32_t startAddr = this->diskBaseAddress_ + sector * FF_MAX_SS;
+	uint32_t size = count * FF_MAX_SS;
+	this->read(startAddr, buff, size);
+	return RES_OK;
+}
+
+
+
+DRESULT IVoidMemory::diskWrite(const BYTE* buff, DWORD sector, UINT count)
+{
+	if (this->diskStatus_ & STA_NOINIT)
+		return RES_NOTRDY;
+	uint32_t startAddr = this->diskBaseAddress_ + sector * FF_MAX_SS;
+	uint32_t size = count * FF_MAX_SS;
+	this->write(startAddr, (uint8_t*)buff, size);
+	return RES_OK;
+}
+
+
+
+DRESULT IVoidMemory::diskIoctl(BYTE cmd, void* buff)
+{
+	if (this->diskStatus_ & STA_NOINIT)
+		return RES_NOTRDY;
+	DRESULT res = RES_ERROR;
+	switch (cmd)
+	{
+		case CTRL_SYNC:	/* Make sure that no pending write process */
+			res = RES_OK;
+			break;
+
+		case GET_SECTOR_COUNT:	/* Get number of sectors on the disk (DWORD) */
+			*(DWORD *) buff = this->diskSize_ / FF_MAX_SS;
+			res = RES_OK;
+			break;
+
+		case GET_SECTOR_SIZE:	/* Get R/W sector size (WORD) */
+			*(WORD *) buff = FF_MAX_SS;
+			res = RES_OK;
+			break;
+
+		case GET_BLOCK_SIZE:/* Get erase block size in unit of sector (DWORD) */
+			*(DWORD *) buff = this->diskBlockSize_;
+			res = RES_OK;
+			break;
+
+		default:
+			res = RES_PARERR;
+			break;
+	}
+
+	return res;
+}
+
+#endif
+
+}
+
+
